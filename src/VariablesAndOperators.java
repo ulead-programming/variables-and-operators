c0dd985cb1d2dfdcae5373fc069c564be095ca80
Pedro Guzman <pedro@subvertic.com>
2020-09-18 21:11:36 -0600

    Ejemplos de variables vistos en clase

---
+++ src/VariablesAndOperators.java
@@ -0,0 +1,27 @@
+public class VariablesAndOperators {
+
+    public static void main(String[] args) {
+
+
+        // Tipos de datos primitivos numéricos enteros
+        short shortNumber = Short.MAX_VALUE;  // Declaración de número entero de 16 bits con signo.
+        int numberOfStudents = 0; // Declaración de número entero de 32 bit con signo.
+        long longNumber = 0;  // Declaración de número entero de 64 bits con signo.
+
+        // Tipos de datos primitivos no enteros
+        float floatNonIntegerNumber = 256.15f;  // Estos son números no enteros de 32 bits.
+        double doubleNonIntegerNumber = 1.458979d; // Estos son números no enteros de 64 bits.
+
+        // Tipos de datos de caracter
+        char letter = 'a'; // El tamaño de los caracteres en memoria va a depender de algo que se conoce como el encoding
+
+
+        // Booleanos que utilizan para almacenar valores de verdad (Verdadero / Falso
+
+        boolean weAreInAProgrammingClass = true;
+        boolean todayIsSaturday = false;
+
+
+    }
+
+}
